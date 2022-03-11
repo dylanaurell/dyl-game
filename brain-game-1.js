f8513691528b9be662113cb35da26441cba94e66
dylanaurell <96631326+dylanaurell@users.noreply.github.com>
2022-03-11 09:37:01 +0700

    Update brain-game-1.js

--- brain-game-1.js
+++ brain-game-1.js
@@ -1,28 +1,3 @@
-//body
-window.onbeforeunload = function () {
-window.scrollTo(0,0);
-}
-function dark_mode(){
-	document.getElementById("dark-mode-off").classList.toggle("dark-mode-close");
-	document.getElementById("dark-mode-on").classList.toggle("dark-mode-open");
-}
-
-
-//menu
-function menu_open(){
-	document.getElementById("menu").classList.add("menu-animation");
-	document.getElementById("menu-outside").style.display = "block";
-}
-function menu_close(){
-	document.getElementById("menu").classList.remove("menu-animation");
-	document.getElementById("menu-outside").style.display = "none";
-}
-function dark_mode(){
-	document.getElementById("dark-mode-off").classList.toggle("dark-mode-close");
-	document.getElementById("dark-mode-on").classList.toggle("dark-mode-open");
-}
-
-
 //game
 function start(){
 	document.getElementById("g-start").style.display="none";
