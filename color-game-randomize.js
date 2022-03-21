8fd5cce19606a2732b031b67d0e86925f8665578
dylanaurell <96631326+dylanaurell@users.noreply.github.com>
2022-03-21 18:25:04 +0700

    Add files via upload

---
+++ color-game-randomize.js
@@ -0,0 +1,150 @@
+function generatenumber_2() {
+	const number_2 = "12"
+	return number_2[Math.floor(Math.random() * number_2.length)]
+}
+
+let gn_2_1_t = generatenumber_2()
+let gn_2_1_l = generatenumber_2()
+let gn_2_2_t = generatenumber_2()
+let gn_2_2_l = generatenumber_2()
+let gn_2_3_t = generatenumber_2()
+let gn_2_3_l = generatenumber_2()
+let gn_2_4_t = generatenumber_2()
+let gn_2_4_l = generatenumber_2()
+let gn_2_5_t = generatenumber_2()
+let gn_2_5_l = generatenumber_2()
+
+let l_2_1_t = "b2t-" + gn_2_1_t
+let l_2_1_l = "b2l-" + gn_2_1_l
+let l_2_2_t = "b2t-" + gn_2_2_t
+let l_2_2_l = "b2l-" + gn_2_2_l
+let l_2_3_t = "b2t-" + gn_2_3_t
+let l_2_3_l = "b2l-" + gn_2_3_l
+let l_2_4_t = "b2t-" + gn_2_4_t
+let l_2_4_l = "b2l-" + gn_2_4_l
+let l_2_5_t = "b2t-" + gn_2_5_t
+let l_2_5_l = "b2l-" + gn_2_5_l
+
+function generatenumber_3() {
+	const number_3 = "123"
+	return number_3[Math.floor(Math.random() * number_3.length)]
+}
+
+let gn_3_1_t = generatenumber_3()
+let gn_3_1_l = generatenumber_3()
+let gn_3_2_t = generatenumber_3()
+let gn_3_2_l = generatenumber_3()
+let gn_3_3_t = generatenumber_3()
+let gn_3_3_l = generatenumber_3()
+let gn_3_4_t = generatenumber_3()
+let gn_3_4_l = generatenumber_3()
+let gn_3_5_t = generatenumber_3()
+let gn_3_5_l = generatenumber_3()
+
+let l_3_1_t = "b3t-" + gn_3_1_t
+let l_3_1_l = "b3l-" + gn_3_1_l
+let l_3_2_t = "b3t-" + gn_3_2_t
+let l_3_2_l = "b3l-" + gn_3_2_l
+let l_3_3_t = "b3t-" + gn_3_3_t
+let l_3_3_l = "b3l-" + gn_3_3_l
+let l_3_4_t = "b3t-" + gn_3_4_t
+let l_3_4_l = "b3l-" + gn_3_4_l
+let l_3_5_t = "b3t-" + gn_3_5_t
+let l_3_5_l = "b3l-" + gn_3_5_l
+
+function generatenumber_4() {
+	const number_4 = "1234"
+	return number_4[Math.floor(Math.random() * number_4.length)]
+}
+
+let gn_4_1_t = generatenumber_4()
+let gn_4_1_l = generatenumber_4()
+let gn_4_2_t = generatenumber_4()
+let gn_4_2_l = generatenumber_4()
+let gn_4_3_t = generatenumber_4()
+let gn_4_3_l = generatenumber_4()
+let gn_4_4_t = generatenumber_4()
+let gn_4_4_l = generatenumber_4()
+let gn_4_5_t = generatenumber_4()
+let gn_4_5_l = generatenumber_4()
+
+let l_4_1_t = "b4t-" + gn_4_1_t
+let l_4_1_l = "b4l-" + gn_4_1_l
+let l_4_2_t = "b4t-" + gn_4_2_t
+let l_4_2_l = "b4l-" + gn_4_2_l
+let l_4_3_t = "b4t-" + gn_4_3_t
+let l_4_3_l = "b4l-" + gn_4_3_l
+let l_4_4_t = "b4t-" + gn_4_4_t
+let l_4_4_l = "b4l-" + gn_4_4_l
+let l_4_5_t = "b4t-" + gn_4_5_t
+let l_4_5_l = "b4l-" + gn_4_5_l
+
+function generatenumber_5() {
+	const number_5 = "12345"
+	return number_5[Math.floor(Math.random() * number_5.length)]
+}
+
+let gn_5_1_t = generatenumber_5()
+let gn_5_1_l = generatenumber_5()
+let gn_5_2_t = generatenumber_5()
+let gn_5_2_l = generatenumber_5()
+let gn_5_3_t = generatenumber_5()
+let gn_5_3_l = generatenumber_5()
+let gn_5_4_t = generatenumber_5()
+let gn_5_4_l = generatenumber_5()
+let gn_5_5_t = generatenumber_5()
+let gn_5_5_l = generatenumber_5()
+
+let l_5_1_t = "b5t-" + gn_5_1_t
+let l_5_1_l = "b5l-" + gn_5_1_l
+let l_5_2_t = "b5t-" + gn_5_2_t
+let l_5_2_l = "b5l-" + gn_5_2_l
+let l_5_3_t = "b5t-" + gn_5_3_t
+let l_5_3_l = "b5l-" + gn_5_3_l
+let l_5_4_t = "b5t-" + gn_5_4_t
+let l_5_4_l = "b5l-" + gn_5_4_l
+let l_5_5_t = "b5t-" + gn_5_5_t
+let l_5_5_l = "b5l-" + gn_5_5_l
+
+window.onload = function() {
+	document.getElementById('b-2-1').classList.add(l_2_1_t);
+	document.getElementById('b-2-1').classList.add(l_2_1_l);
+	document.getElementById('b-2-2').classList.add(l_2_2_t);
+	document.getElementById('b-2-2').classList.add(l_2_2_l);
+	document.getElementById('b-2-3').classList.add(l_2_3_t);
+	document.getElementById('b-2-3').classList.add(l_2_3_l);
+	document.getElementById('b-2-4').classList.add(l_2_4_t);
+	document.getElementById('b-2-4').classList.add(l_2_4_l);
+	document.getElementById('b-2-5').classList.add(l_2_5_t);
+	document.getElementById('b-2-5').classList.add(l_2_5_l);
+	document.getElementById('b-3-1').classList.add(l_3_1_t);
+	document.getElementById('b-3-1').classList.add(l_3_1_l);
+	document.getElementById('b-3-2').classList.add(l_3_2_t);
+	document.getElementById('b-3-2').classList.add(l_3_2_l);
+	document.getElementById('b-3-3').classList.add(l_3_3_t);
+	document.getElementById('b-3-3').classList.add(l_3_3_l);
+	document.getElementById('b-3-4').classList.add(l_3_4_t);
+	document.getElementById('b-3-4').classList.add(l_3_4_l);
+	document.getElementById('b-3-5').classList.add(l_3_5_t);
+	document.getElementById('b-3-5').classList.add(l_3_5_l);
+	document.getElementById('b-4-1').classList.add(l_4_1_t);
+	document.getElementById('b-4-1').classList.add(l_4_1_l);
+	document.getElementById('b-4-2').classList.add(l_4_2_t);
+	document.getElementById('b-4-2').classList.add(l_4_2_l);
+	document.getElementById('b-4-3').classList.add(l_4_3_t);
+	document.getElementById('b-4-3').classList.add(l_4_3_l);
+	document.getElementById('b-4-4').classList.add(l_4_4_t);
+	document.getElementById('b-4-4').classList.add(l_4_4_l);
+	document.getElementById('b-4-5').classList.add(l_4_5_t);
+	document.getElementById('b-4-5').classList.add(l_4_5_l);
+	document.getElementById('b-5-1').classList.add(l_5_1_t);
+	document.getElementById('b-5-1').classList.add(l_5_1_l);
+	document.getElementById('b-5-2').classList.add(l_5_2_t);
+	document.getElementById('b-5-2').classList.add(l_5_2_l);
+	document.getElementById('b-5-3').classList.add(l_5_3_t);
+	document.getElementById('b-5-3').classList.add(l_5_3_l);
+	document.getElementById('b-5-4').classList.add(l_5_4_t);
+	document.getElementById('b-5-4').classList.add(l_5_4_l);
+	document.getElementById('b-5-5').classList.add(l_5_5_t);
+	document.getElementById('b-5-5').classList.add(l_5_5_l);
+}
---
+++ color-game.js
@@ -0,0 +1,291 @@
+//start
+function start() {
+	document.getElementById('layer-start').classList.add('hide');
+}
+function go() {
+	document.getElementById('layer-question').classList.add('hide');
+}
+
+//2
+function g_2() {
+	document.getElementById('layer-2x2').classList.add('hide');
+}
+function n_2_1() {
+	document.getElementById('layer-2-1').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-2-1').classList.add('hide') ;}, 1000);
+}
+function y_2_1() {
+	document.getElementById('layer-2-1').classList.add('opacity') ;
+	document.getElementById('b-2-1').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-2-1').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}
+function n_2_2() {
+	document.getElementById('layer-2-2').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-2-2').classList.add('hide') ;}, 1000);
+}
+function y_2_2() {
+	document.getElementById('layer-2-2').classList.add('opacity') ;
+	document.getElementById('b-2-2').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-2-2').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}
+function n_2_3() {
+	document.getElementById('layer-2-3').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-2-3').classList.add('hide') ;}, 1000);
+}
+function y_2_3() {
+	document.getElementById('layer-2-3').classList.add('opacity') ;
+	document.getElementById('b-2-3').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-2-3').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}
+function n_2_4() {
+	document.getElementById('layer-2-4').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-2-4').classList.add('hide') ;}, 1000);
+}
+function y_2_4() {
+	document.getElementById('layer-2-4').classList.add('opacity') ;
+	document.getElementById('b-2-4').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-2-4').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}
+function n_2_5() {
+	document.getElementById('layer-2-5').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-2-5').classList.add('hide') ;}, 1000);
+}
+function y_2_5() {
+	document.getElementById('layer-2-5').classList.add('opacity') ;
+	document.getElementById('b-2-5').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-2-5').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}
+
+//3
+function g_3() {
+	document.getElementById('layer-3x3').classList.add('hide');
+}
+function n_3_1() {
+	document.getElementById('layer-3-1').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-3-1').classList.add('hide') ;}, 1000);
+}
+function y_3_1() {
+	document.getElementById('layer-3-1').classList.add('opacity') ;
+	document.getElementById('b-3-1').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-3-1').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}
+function n_3_2() {
+	document.getElementById('layer-3-2').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-3-2').classList.add('hide') ;}, 1000);
+}
+function y_3_2() {
+	document.getElementById('layer-3-2').classList.add('opacity') ;
+	document.getElementById('b-3-2').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-3-2').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}
+function n_3_3() {
+	document.getElementById('layer-3-3').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-3-3').classList.add('hide') ;}, 1000);
+}
+function y_3_3() {
+	document.getElementById('layer-3-3').classList.add('opacity') ;
+	document.getElementById('b-3-3').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-3-3').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}
+function n_3_4() {
+	document.getElementById('layer-3-4').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-3-4').classList.add('hide') ;}, 1000);
+}
+function y_3_4() {
+	document.getElementById('layer-3-4').classList.add('opacity') ;
+	document.getElementById('b-3-4').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-3-4').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}
+function n_3_5() {
+	document.getElementById('layer-3-5').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-3-5').classList.add('hide') ;}, 1000);
+}
+function y_3_5() {
+	document.getElementById('layer-3-5').classList.add('opacity') ;
+	document.getElementById('b-3-5').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-3-5').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}
+
+//4
+function g_4() {
+	document.getElementById('layer-4x4').classList.add('hide');
+}
+function n_4_1() {	
+	document.getElementById('layer-4-1').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-4-1').classList.add('hide') ;}, 1000);
+}	
+function y_4_1() {	
+	document.getElementById('layer-4-1').classList.add('opacity') ;
+	document.getElementById('b-4-1').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-4-1').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1
+	document.getElementById("result").innerHTML = score + "/20";
+}	
+function n_4_2() {	
+	document.getElementById('layer-4-2').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-4-2').classList.add('hide') ;}, 1000);
+}	
+function y_4_2() {	
+	document.getElementById('layer-4-2').classList.add('opacity') ;
+	document.getElementById('b-4-2').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-4-2').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}	
+function n_4_3() {	
+	document.getElementById('layer-4-3').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-4-3').classList.add('hide') ;}, 1000);
+}	
+function y_4_3() {	
+	document.getElementById('layer-4-3').classList.add('opacity') ;
+	document.getElementById('b-4-3').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-4-3').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}	
+function n_4_4() {	
+	document.getElementById('layer-4-4').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-4-4').classList.add('hide') ;}, 1000);
+}	
+function y_4_4() {	
+	document.getElementById('layer-4-4').classList.add('opacity') ;
+	document.getElementById('b-4-4').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-4-4').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}	
+function n_4_5() {	
+	document.getElementById('layer-4-5').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-4-5').classList.add('hide') ;}, 1000);
+}	
+function y_4_5() {	
+	document.getElementById('layer-4-5').classList.add('opacity') ;
+	document.getElementById('b-4-5').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-4-5').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+}
+
+//5
+function g_5() {
+	document.getElementById('layer-5x5').classList.add('hide');
+}
+function n_5_1() {	
+	document.getElementById('layer-5-1').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-5-1').classList.add('hide') ;}, 1000);
+	clearInterval(intervalop);
+}	
+function y_5_1() {	
+	document.getElementById('layer-5-1').classList.add('opacity') ;
+	document.getElementById('b-5-1').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-5-1').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+	clearInterval(intervalop);
+}	
+function n_5_2() {	
+	document.getElementById('layer-5-2').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-5-2').classList.add('hide') ;}, 1000);
+	clearInterval(intervalop);
+}	
+function y_5_2() {	
+	document.getElementById('layer-5-2').classList.add('opacity') ;
+	document.getElementById('b-5-2').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-5-2').classList.add('hide') ;}, 1000);
+	clearInterval(intervalop);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+	clearInterval(intervalop);
+}	
+function n_5_3() {	
+	document.getElementById('layer-5-3').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-5-3').classList.add('hide') ;}, 1000);
+	clearInterval(intervalop);
+}	
+function y_5_3() {	
+	document.getElementById('layer-5-3').classList.add('opacity') ;
+	document.getElementById('b-5-3').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-5-3').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+	clearInterval(intervalop);
+}	
+function n_5_4() {	
+	document.getElementById('layer-5-4').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-5-4').classList.add('hide') ;}, 1000);
+	clearInterval(intervalop);
+}	
+function y_5_4() {	
+	document.getElementById('layer-5-4').classList.add('opacity') ;
+	document.getElementById('b-5-4').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-5-4').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+	clearInterval(intervalop);
+}	
+function n_5_5() {	
+	document.getElementById('layer-5-5').classList.add('opacity') ;
+	setTimeout(function() {document.getElementById('layer-5-5').classList.add('hide') ;}, 1000);
+	clearInterval(intervalop);
+}	
+function y_5_5() {	
+	document.getElementById('layer-5-5').classList.add('opacity') ;
+	document.getElementById('b-5-5').classList.add('hide') ;
+	setTimeout(function() {document.getElementById('layer-5-5').classList.add('hide') ;}, 1000);
+	var score_after = document.getElementById("result").innerHTML;
+	var score = parseInt(score_after,10) + 1;
+	document.getElementById("result").innerHTML = score + "/20";
+	clearInterval(intervalop);
+}	
+
+var second = 0;
+var minute = 0;
+
+function start_timer() {
+    intervalop = setInterval(function(){
+    	second = second + 1
+        document.getElementById('jam').innerHTML = "0" + minute + ":" + "0" + second ;
+        if (second > 9){document.getElementById('jam').innerHTML = "0" + minute + ":" + second ;}
+		if (second > 59){second = 0 ; minute = minute + 1 ; document.getElementById('jam').innerHTML = "0" + minute + ":" + second ;}
+		if (minute > 9){document.getElementById('jam').innerHTML = minute + ":" + second ;}
+	}, 1000);
+}
